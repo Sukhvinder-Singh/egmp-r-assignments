975a7ddfc8288b67e0b0b0b8e510541fb054d23f
Sukhvinder Singh <sukhvisukh@gmail.com>
2023-10-21 10:27:10 +0530

    Part 4

--- class-23-10-21.r
+++ class-23-10-21.r
@@ -28,13 +28,26 @@ tree_test <- dt2[-index, ]
 nrow(tree_train)
 nrow(tree_test)
 
+# Model
 model_tree_a <- lm(Volume ~ Girth, data = tree_train)
 summary(model_tree_a)
 
 model_tree_b <- lm(Volume ~ Girth + Height, data = tree_train)
 summary(model_tree_b)
 
+# Prediction
 pred_tree_a <- predict(model_tree_a, tree_test)
 pred_tree_b <- predict(model_tree_b, tree_test)
 
 mse_a <- sum((pred_tree_a - tree_test$Volume)^2) / nrow(tree_test)
+mse_b <- sum((pred_tree_b - tree_test$Volume)^2) / nrow(tree_test)
+
+mse_a
+mse_b
+
+# Lower the value, better the fit, and vice-versa
+AIC(model_tree_a, model_tree_b)
+BIC(model_tree_a, model_tree_b)
+
+plot(model_tree_a, 1)
+plot(model_tree_a, 2)
