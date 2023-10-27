dd55c3dca99aac7380553665fd68492a18854275
Sukhvinder Singh <sukhvisukh@gmail.com>
2023-10-28 02:12:51 +0530

    modelled data

--- assignment-4.r
+++ assignment-4.r
@@ -14,3 +14,73 @@
 
 # Set terminal width
 options("width" = 500)
+
+# Standardizing values
+custom_std <- function(x) {
+  x <- (x - mean(x)) / sd(x)
+  return(x)
+}
+
+# This gets the working directory
+working_dir <- getwd()
+# This line replaces windows forward slash with backslash
+working_dir_path <- gsub("\\", "/", working_dir, fixed = TRUE)
+
+# We set the working directory to read the files conveniently
+setwd(working_dir_path)
+
+placement_data <- read.csv(
+  paste(working_dir_path, "assignment-4-data/placement.csv", sep = "/"),
+  header = TRUE
+)
+
+placement_data <- placement_data[!is.na(placement_data$salary), ]
+placement_data <- placement_data[
+  ,
+  names(placement_data) %in%
+    c("ssc_p", "hsc_p", "degree_p", "etest_p", "mba_p", "salary")
+]
+
+placement_data
+
+placement_data_norm <- data.frame(lapply(placement_data, custom_std))
+placement_data_norm
+
+index <- sample(
+  seq_len(nrow(placement_data_norm)),
+  round(0.7 * nrow(placement_data_norm))
+)
+
+placement_train <- placement_data_norm[index, ]
+placement_test <- placement_data_norm[-index, ]
+
+nrow(placement_train)
+nrow(placement_test)
+
+# Model
+model_a <- lm(salary ~ mba_p, data = placement_train)
+model_b <- lm(salary ~ mba_p + degree_p + hsc_p, data = placement_train)
+
+summary(model_a)
+summary(model_b)
+
+pred_a <- predict(model_a, placement_test)
+pred_b <- predict(model_b, placement_test)
+
+mse_a <- sum((pred_a - placement_test$salary)^2) / nrow(placement_test)
+mse_b <- sum((pred_b - placement_test$salary)^2) / nrow(placement_test)
+
+mse_a
+mse_b
+
+# Lower the value, better the fit, and vice-versa
+AIC(model_a, model_b)
+BIC(model_a, model_b)
+
+# Various plots of model A
+plot(model_a, 1)
+plot(model_a, 2)
+plot(model_a, 3)
+plot(model_a, 4)
+plot(model_a, 5)
+plot(model_a, 6)
