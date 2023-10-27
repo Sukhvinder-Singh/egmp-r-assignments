70933c8295ff9b9c1d2b060d87ce814d85c427ea
Sukhvinder Singh <sukhvisukh@gmail.com>
2023-10-28 03:04:54 +0530

    Documented the code

--- assignment-4.r
+++ assignment-4.r
@@ -12,10 +12,21 @@
 # single folder, and can be read with the code below, as we are dynamically
 # getting the working directory using R code.
 
+# In this example, we are trying to model data from file:
+# "placement.csv" in assignment-4-data folder. It has several columns such as
+# scores obtained in high school, college, placement marks, MBA score etc,
+# and we are trying to model and test the data to find out the relationship
+# between them and the salary of the individual using linear regression.
+# This will give us insights about whether marks in MBA/College or the
+# pattern of scores obtained during schooling along with college/MBA/placement
+# test marks, will have effect on # the salary/package/CTC of the individual
+
+# All steps we followed for this assignment are also included as comments below,
+
 # Set terminal width
 options("width" = 500)
 
-# Standardizing values
+# Standardizing values function
 custom_std <- function(x) {
   x <- (x - mean(x)) / sd(x)
   return(x)
@@ -29,44 +40,62 @@ working_dir_path <- gsub("\\", "/", working_dir, fixed = TRUE)
 # We set the working directory to read the files conveniently
 setwd(working_dir_path)
 
+# Reading the data file
 placement_data <- read.csv(
   paste(working_dir_path, "assignment-4-data/placement.csv", sep = "/"),
   header = TRUE
 )
 
+# We found some of the salary cells were empty/not available
+# Thus, we have removed those rows in which salary was not present.
+# This was the first step in cleansing the data for further analysis.
 placement_data <- placement_data[!is.na(placement_data$salary), ]
+
+# We also removed unnecessary variables that were
+# not required for linear regression
 placement_data <- placement_data[
   ,
   names(placement_data) %in%
     c("ssc_p", "hsc_p", "degree_p", "etest_p", "mba_p", "salary")
 ]
 
-placement_data
-
+# Normalizing the placement data
 placement_data_norm <- data.frame(lapply(placement_data, custom_std))
-placement_data_norm
 
+# Getting a sample of the placement data
 index <- sample(
   seq_len(nrow(placement_data_norm)),
   round(0.7 * nrow(placement_data_norm))
 )
 
+# Training set of placement data
 placement_train <- placement_data_norm[index, ]
+
+# Testing set of placement data
 placement_test <- placement_data_norm[-index, ]
 
 nrow(placement_train)
 nrow(placement_test)
 
-# Model
+# Modeling the placement data. Here salary is the
+# dependent variable, and we model the
+# data with various predictor variables
 model_a <- lm(salary ~ mba_p, data = placement_train)
 model_b <- lm(salary ~ mba_p + degree_p + hsc_p, data = placement_train)
 
+# We get the summary of both models and check
+# various coefficients to check if everything is
+# correct till now
 summary(model_a)
 summary(model_b)
 
+# This is for prediction with both models we
+# have made above with placement test dataset
 pred_a <- predict(model_a, placement_test)
 pred_b <- predict(model_b, placement_test)
 
+# We check the mean square error to find out
+# which model may be better than others
 mse_a <- sum((pred_a - placement_test$salary)^2) / nrow(placement_test)
 mse_b <- sum((pred_b - placement_test$salary)^2) / nrow(placement_test)
 
@@ -77,7 +106,8 @@ mse_b
 AIC(model_a, model_b)
 BIC(model_a, model_b)
 
-# Various plots of model A
+# As per our results, model A is better, hence we
+# plot various graphs for interpretation.
 plot(model_a, 1)
 plot(model_a, 2)
 plot(model_a, 3)
