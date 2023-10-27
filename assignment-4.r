63428e5ff17e3f0800e7104e2c3d7610bd07bdf0
Sukhvinder Singh <sukhvisukh@gmail.com>
2023-10-27 21:45:55 +0530

    Starting assignment 4

---
+++ assignment-4.r
@@ -0,0 +1,16 @@
+# R assignment submitted by group members:
+# 1. Sukhvinder Singh (kpmg03_sukhvinder_s@mdi.ac.in)
+# 2. Kritika Seth (kpmg03_kritika_s@mdi.ac.in)
+# 3. Vikram Singh (kpmg03_vikram_s@mdi.ac.in)
+# 4. Parul Gupta (kpmg03_parul_g@mdi.ac.in)
+# 5. Kanchan Nanda (kpmg03_kanchan_n@mdi.ac.in)
+# 6. Ankit Bhatia (kpmg03_ankit_b@mdi.ac.in)
+
+# Notes: We've used the latest R version 4.3.1.
+# We've also commented the file for line-by-line explanation.
+# Also need to set the working directory so that the files are all in a
+# single folder, and can be read with the code below, as we are dynamically
+# getting the working directory using R code.
+
+# Set terminal width
+options("width" = 500)
